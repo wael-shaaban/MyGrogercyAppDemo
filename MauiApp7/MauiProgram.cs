54c9b2f96d8c3a721b2c5f24053605928507d096
Aleraky <waelalerky@gmail.com>
2024-07-30 17:13:30 +0300

    add firstcommint

--- MauiApp7/MauiProgram.cs
+++ MauiApp7/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MauiApp7
 {
@@ -9,6 +10,7 @@ namespace MauiApp7
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                 .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
